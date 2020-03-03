f501ae5b388c4269bb9b1e5e4313343b7c755cee
Vlad Anesov <v.anesov@corp.vk.com>
2020-03-04 01:14:59 +0300

    Updates

---
+++ precache-manifest.dba4ccd27be7bb1763bfcdbbda37a1ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727867cc0d0244d46f0647ec660bbd08",
+    "url": "/upload-selfie/index.html"
+  },
+  {
+    "revision": "5e278c3449388e8616dc",
+    "url": "/upload-selfie/static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "9715fb97521c5b2af078",
+    "url": "/upload-selfie/static/css/main.8610aee6.chunk.css"
+  },
+  {
+    "revision": "5e278c3449388e8616dc",
+    "url": "/upload-selfie/static/js/2.412d7528.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/upload-selfie/static/js/2.412d7528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9715fb97521c5b2af078",
+    "url": "/upload-selfie/static/js/main.4b4bcddd.chunk.js"
+  },
+  {
+    "revision": "653d18ce9e84d08ce930",
+    "url": "/upload-selfie/static/js/runtime-main.98783a0a.js"
+  }
+]);
